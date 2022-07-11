cf0386bb3d7153654f513cf5aff3cd4818392f58
unknown <ponselvam.sakthivel@brickendon.com>
2022-07-11 22:01:58 +0530

    travis scripts added

---
+++ app.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+if [ "$TRAVIS_BRANCH" = "uat" ]
+then
+    CF_SPACE="uat"
+fi
+
+if [ "$TRAVIS_BRANCH" = "training" ]
+then
+    CF_SPACE="training"
+fi
+
+if [ "$TRAVIS_BRANCH" = "testing" ]
+then
+    CF_SPACE="test"
+fi
+
+if [ "$TRAVIS_BRANCH" = "pre-production" ]
+then
+    CF_SPACE="preprod"
+fi
+
+if [ "$TRAVIS_BRANCH" = "sandbox" ]
+then
+    CF_SPACE="sand"
+fi
+
+if [ "$TRAVIS_BRANCH" = "production" ]
+then
+    CF_SPACE="prod"
+fi
+
+echo "$CF_SPACE"
+
+sed "s/CF_SPACE/$CF_SPACE/g" manifest-template.yml > manifest.yml
