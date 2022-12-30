4d07088b044ae97cc55d5df0b90b622c407ea1ec
Brijesh Patel <Brijesh.Patel@brickendon.com>
2022-12-30 18:39:43 +0530

    Task 4227: CON-3163 - AWS SSM/SSO changes

---
+++ Ccs.Ppg.NotificationService/Services/IServices/IAwsParameterStoreService.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.SimpleSystemsManagement.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ccs.Ppg.NotificationService.Services.IServices
+{
+  public interface IAwsParameterStoreService
+  {
+    Task<List<Parameter>> GetParameters(string path);
+
+    string FindParameterByName(List<Parameter> parameters, string name);
+
+    KeyValuePair<string, string> GetParameter(List<Parameter> parameters, string name, string key);
+
+    List<KeyValuePair<string, string>> GetParameterFromCommaSeparated(List<Parameter> parameters, string name, string key);
+  }
+}
