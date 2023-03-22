c70c18b3cfdee2e3c75f7d0aad9676395868bac1
VijayHirudayasamy-bc <vijay.hirudayasamy@brickendon.com>
2023-03-22 14:35:17 +0000

    Changes applied to create delay the email based on the Auth 0 user creation.

---
+++ Ccs.Ppg.NotificationService/Model/EmailResponseInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ccs.Ppg.NotificationService.Model
+{
+  public class EmailResponseInfo
+  {
+    public EmailInfo EmailInfo { get; set; }
+    public bool IsUserInAuth0 { get; set; }
+  }
+}
