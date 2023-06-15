87d280a6378fab3620d36bb02bb582079ef84c37
SARANYA <SARANYA@DESKTOP-1G4R5OO>
2023-06-15 15:33:33 +0530

    ChangesUpdated-new-email-api

--- Ccs.Ppg.NotificationService.API/Controllers/NotificationController.cs
+++ Ccs.Ppg.NotificationService.API/Controllers/NotificationController.cs
@@ -46,14 +46,38 @@ namespace Ccs.Ppg.NotificationService.API.Controllers
         public async Task<bool> Post(MessageRequestModel message)
         {
             return await _messageProviderService.SendMessage(message);
-        }
+		    }
+
+		    /// <summary>
+		    /// Allows a user to send email
+		    /// </summary>
+		    /// <response  code="200">Ok</response>
+		    /// <response  code="401">Unauthorised</response>
+		    /// <response  code="403">Forbidden</response>
+		    /// <response  code="404">Not found</response>
+		    /// <response  code="400">Bad request. </response>
+		    /// <remarks>
+		    /// Sample request:
+		    ///
+		    ///     POST /notification/email
+		    ///        {
+		    ///        "to": "username@xxxx.com",
+		    ///        "templateId": "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+		    ///        "bodyContent": {
+		    ///                 "email": "UserName@xxxx.com",
+		    ///                 "additionalProp3": "string",
+		    ///                 "additionalProp3": "string"
+        ///                 }
+		    ///         }
+		    ///
+		    /// </remarks>
 
 		    [HttpPost("email")]
 		    [SwaggerOperation(Tags = new[] { "notification/email" })]
 		    [ProducesResponseType(typeof(bool), 200)]
 	      public async Task SendEmailAsync(EmailInfo emailInfo)
 		    {
-             await _emailProviderService.SendEmailAsync(emailInfo);
+          await _emailProviderService.SendEmailAsync(emailInfo);
         }			      
 		}
-	}
+}
--- Ccs.Ppg.NotificationService/Model/EmailInfo.cs
+++ Ccs.Ppg.NotificationService/Model/EmailInfo.cs
@@ -4,8 +4,10 @@ namespace Ccs.Ppg.NotificationService.Model
 {
 	public class EmailInfo
 	{
+		[Required(ErrorMessage = "Email is required Ex:UserName@example.com")]
 		public string To { get; set; }
 
+		[Required(ErrorMessage = "Email template is required")]
 		public string TemplateId { get; set; }
 
 		public Dictionary<string, dynamic> BodyContent { get; set; }
