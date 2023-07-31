64ec778f1b7ab276c79bd0e0d82ef976da47aee8
Jayaram Seetharaman <jayaram.seetharaman@brickendon.com>
2023-07-31 16:17:00 +0530

    Fixing existing issues in notification endpoint

--- Ccs.Ppg.NotificationService/Services/IServices/IEmailProviderService.cs
+++ Ccs.Ppg.NotificationService/Services/IServices/IEmailProviderService.cs
@@ -9,6 +9,6 @@ namespace Ccs.Ppg.NotificationService.Services.IServices
 {
 	public interface IEmailProviderService
 	{
-		Task SendEmailAsync(EmailInfo emailInfo);
+		Task<bool> SendEmailAsync(EmailInfo emailInfo);
 	}
 }
